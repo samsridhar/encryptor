ba1d0d8881d189d9ec1b871d8960b077079f4493
samsridhar <samsridhar.mac@gmail.com>
2017-03-18 15:36:34 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,7 @@ $(function(){
 
 		file = e.target.files[0];
 
-		if(file.size > 1024*1024){
+		if(file.size > 1024*1024*1024*1024){
 			alert('Please choose files smaller than 1mb, otherwise you may crash your browser. \nThis is a known issue. See the tutorial.');
 			return;
 		}
